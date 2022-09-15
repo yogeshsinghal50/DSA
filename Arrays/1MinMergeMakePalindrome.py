1af578ed373cef5a102ee3fc84c79d264ecebc3a
Yogesh Singhal <ysinghal1155@gmail.com>
2022-09-15 17:39:03 +0530

    push

---
+++ Arrays/1MinMergeMakePalindrome.py
@@ -0,0 +1,9 @@
+def findMin(arr:List[int]) -> int:
+    return 0
+
+
+if __name__ == '__main__':
+ 
+    arr = [6, 1, 4, 3, 1, 7]
+    min = findMin(arr)
+    print("The minimum number of operations required:", min)
